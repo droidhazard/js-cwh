2783a1dd9fc6b043a21fecc5039a3f78e9a722c8
Zohaib Ali <www.zohaib.0335@gmail.com>
2022-11-20 20:24:14 +0500

    add files

---
+++ table-links.js
@@ -0,0 +1,8 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t.rows)
+console.log(t.caption.innerText)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.rows[0].innerText)
+console.log(t.rows[0].rowIndex)
