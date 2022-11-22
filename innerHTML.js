947e79c3fbfb2d1f669366947039c3849a7f6199
Zohaib Ali <www.zohaib.0335@gmail.com>
2022-11-22 14:40:20 +0500

    upload file

--- innerHTML.js
+++ innerHTML.js
@@ -1,2 +1,13 @@
 console.dir(document.getElementsByTagName('span')[0])
 console.log(document.body.firstChild.nodeName)
+console.log(document.body.firstElementChild.nodeName)
+
+let x = document.getElementsByTagName('span')[0]
+console.log(x)
+console.dir(x)
+console.log(first.innerHTML)
+console.log(first.outerHTML)
+console.log(document.body.firstChild.data)
+console.log(document.body.firstChild.nodeValue)
+console.log(document.body.textContent)
+document.getElementById("hidden").hidden = false
