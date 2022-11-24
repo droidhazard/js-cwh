bf8d6f6608b15a432098b21b6a652054ce0f5465
Zohaib Ali <www.zohaib.0335@gmail.com>
2022-11-24 20:31:59 +0500

    add file

--- ch8-ps.js
+++ ch8-ps.js
@@ -13,3 +13,30 @@ promptBtn.addEventListener('click', (showPrompt) => {
 confirmBtn.addEventListener('click', (showConfirm) => {
   confirm('I am a confirmation box')
 })
+
+// Question 2
+
+let googleBtn = document.getElementById('googleBtn')
+let youtubeBtn = document.getElementById('youtubeBtn')
+let facebookBtn = document.getElementById('facebookBtn')
+let gmailBtn = document.getElementById('gmailBtn')
+let githubBtn = document.getElementById('githubBtn')
+
+// Question 3
+
+// const fetchContent = async (url) => {
+//   con = await fetch(url);
+//   let a = await con.json()
+//   return a;
+// }
+// setInterval(async function() {
+//   let url = 'https://jsonplaceholder.typicode.com/todos/1'
+//   console.log(await fetchContent(url))
+// }, 3000)
+
+// Question 4
+let toggleBtn = document.getElementById('bulbToggle')
+let bulb = document.getElementsByClassName('bulb-head')[0]
+toggleBtn.addEventListener('click', ()=> {
+  bulb.classList.toggle('bulb-lit')
+})
