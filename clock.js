599e8d8444b520165d00f2a8a12da5f0182da4b5
Zohaib Ali <www.zohaib.0335@gmail.com>
2022-11-24 22:17:11 +0500

    fix clock starting counting time relatively from start

--- clock.js
+++ clock.js
@@ -1,19 +1,17 @@
 let dateObj = new Date()
-let hh = dateObj.getHours()
-let mm = dateObj.getMinutes()
+let initialHour = dateObj.getHours()
+let initialMinute = dateObj.getMinutes()
 let ss = dateObj.getSeconds()
-console.log(hh, mm, ss)
+console.log(initialHour, initialMinute, ss)
 let digitalClock = document.getElementsByClassName('digital-clock')[0]
-let startClock = document.getElementById('start-clock')
-let stopClock = document.getElementById('stop-clock')
 let secondHand = document.getElementById('second-hand')
 let minuteHand = document.getElementById('minute-hand')
 let hourHand = document.getElementById('hour-hand')
 
 // initial setup
 let secondsRotation = ss * 6
-let minutesRotation = mm * 6
-let hoursRotation = hh * 30
+let minutesRotation = initialMinute * 6
+let hoursRotation = initialHour * 30
 console.log(secondsRotation)
 secondHand.style.rotate = `${secondsRotation}deg`
 minuteHand.style.rotate = `${minutesRotation}deg`
@@ -25,27 +23,19 @@ hourHand.style.rotate = `${hoursRotation}deg`
 setInterval(seconds = () => {
   dateObj = new Date()
   ss = dateObj.getSeconds()
+  mm = dateObj.getMinutes()
+  hh = dateObj.getHours()
   secondsRotation += 6;
   secondHand.style.rotate = `${secondsRotation}deg`
   digitalClock.innerText = `${hh}:${mm}:${ss}`
+  if (mm != initialMinute) {
+    minutesRotation += 6;
+    minuteHand.style.rotate = `${minutesRotation}deg`
+    initialMinute = mm;
+  } else { }
+  if (hh != initialHour) {
+    hoursRotation += 30;
+    hourHand.style.rotate = `${hoursRotation}deg`;
+    initialHour = hh;
+  }
 }, 1000)
-setInterval(minutes = () => {
-  dateObj = new Date()
-  mm = dateObj.getMinutes()
-  minutesRotation += 6;
-  minuteHand.style.rotate = `${minutesRotation}deg`
-}, 60000)
-setInterval(hours = () => {
-  dateObj = new Date()
-  hh = dateObj.getHours()
-  hoursRotation += 30;
-  hourHand.style.rotate = `${hoursRotation}deg`;
-}, 3600000)
-
-startClock.addEventListener('click', () => {
-  setInterval(running)
-})
-stopClock.addEventListener('click', () => {
-  console.log('stop')
-  clearInterval(running)
-})
