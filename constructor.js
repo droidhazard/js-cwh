89d5219cfc8aee103b51d600b784fed2a2927797
Zohaib Ali <www.zohaib.0335@gmail.com>
2022-12-03 10:02:13 +0500

    add file

--- constructor.js
+++ constructor.js
@@ -1,4 +1,8 @@
 class Employee {
+  constructor(name) {
+    this.name = name
+    console.log(`${this.name}: Employee's constructor is here`)
+  }
   login() {
     console.log(`Employee has logged in`)
   }
@@ -11,6 +15,14 @@ class Employee {
 }
 
 class Programmer extends Employee {
+  constructor(name) {
+    super(name)
+    this.name = name
+    console.log(`${this.name}: Programmer's constructor is here`)
+  }
+  // constructor(...args) { // if there is no constructor in child class, it will automatically be created
+  //   super(...args)
+  // }
   requestCoffee(x) {
     console.log(`Employee has requested ${x} coffees`)
   }
@@ -21,6 +33,6 @@ class Programmer extends Employee {
   }
 }
 
-let e = new Programmer()
+let e = new Programmer('Zohaib')
 e.login()
 e.requestLeave(3)
