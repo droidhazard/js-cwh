22b8cbd66c12630e4794a44aacad040476c8ecea
Zohaib Ali <www.zohaib.0335@gmail.com>
2022-12-04 20:58:06 +0500

    add file

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+console.log(a)
+
+greet()
+
+// function greet() {
+//   console.log('Hello Good Day')
+// }
+
+ function greet() {
+  console.log('Hello Good Day')
+}
+
+// greet = () => {
+//   console.log('Hello Good Day')
+// }
+var a = 10;
+console.log(a)
